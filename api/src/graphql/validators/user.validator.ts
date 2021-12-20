7deb95c3fc4c4d6c4600d3703a3fb7ab3561b867
fadhilsaheer <fadhilsaheer@gmail.com>
2021-12-20 17:26:57 +0530

    validators for mutations on user

---
+++ api/src/graphql/validators/user.validator.ts
@@ -0,0 +1,20 @@
+import { UserInputError } from 'apollo-server';
+import { LoginRequestArgs, RegisterRequestArgs } from '../../types/request';
+
+export const validateRegisterArgs = (
+  args: RegisterRequestArgs
+): RegisterRequestArgs => {
+  if (!args.email || !args.name || !args.password || !args.profile) {
+    throw new UserInputError('required fields not found');
+  }
+
+  return args;
+};
+
+export const validateLoginArgs = (args: LoginRequestArgs): LoginRequestArgs => {
+  if (!args.email || !args.password) {
+    throw new UserInputError('required fields not found');
+  }
+
+  return args;
+};
