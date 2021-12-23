8b28da78932b7980eb6369834898d666d080e35f
fadhilsaheer <fadhilsaheer@gmail.com>
2021-12-23 20:01:09 +0530

    update user controller

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -1,6 +1,10 @@
 import { UserInputError } from 'apollo-server';
-import { loginUser, registerUser } from '../models/user.model';
-import { LoginRequestArgs, RegisterRequestArgs } from '../types/request';
+import { loginUser, registerUser, updateUser } from '../models/user.model';
+import {
+  LoginRequestArgs,
+  RegisterRequestArgs,
+  UpdateUserRequestArgs,
+} from '../types/request';
 import { generateToken } from '../utils/jwt';
 
 // Takes `registerRequestArgs` and return user data with token
@@ -18,6 +22,21 @@ export const registerController = async (args: RegisterRequestArgs) => {
 // Takes `loginRequestArgs` and return user data with token
 export const loginController = async (args: LoginRequestArgs) => {
   const user: any = await loginUser(args).catch((err) => {
+    throw new UserInputError(err);
+  });
+
+  return {
+    ...user,
+    token: generateToken(user),
+  };
+};
+
+// Takes `UpdateRequestArgs` and return user data with token
+export const updateUserController = async (
+  args: UpdateUserRequestArgs,
+  loggedInUser: { id: number }
+) => {
+  const user: any = await updateUser(args, loggedInUser).catch((err) => {
     throw new UserInputError(err);
   });
 
