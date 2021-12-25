e5a71331bdffe32f49d8412a5909a7f30a251656
fadhilsaheer <fadhilsaheer@gmail.com>
2021-12-25 10:27:19 +0530

    get user products query

--- api/src/graphql/queries/index.ts
+++ api/src/graphql/queries/index.ts
@@ -1,5 +1,9 @@
 import { GraphQLObjectType } from 'graphql';
-import { GET_ALL_PRODUCTS, GET_ONE_PRODUCT } from './product.query';
+import {
+  GET_ALL_PRODUCTS,
+  GET_ALL_USER_PRODUCTS,
+  GET_ONE_PRODUCT,
+} from './product.query';
 import { GET_USER_DETAILS } from './user.query';
 
 export const Queries = new GraphQLObjectType({
@@ -11,5 +15,6 @@ export const Queries = new GraphQLObjectType({
     // PRODUCT
     getAllProducts: GET_ALL_PRODUCTS,
     getOneProduct: GET_ONE_PRODUCT,
+    getAllUserProducts: GET_ALL_USER_PRODUCTS,
   }),
 });
