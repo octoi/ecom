63771901aee5a80fdda2f0c5a7e9ff2df64486fc
fadhilsaheer <fadhilsaheer@gmail.com>
2021-12-25 09:13:08 +0530

    get one product query

--- api/src/graphql/queries/product.query.ts
+++ api/src/graphql/queries/product.query.ts
@@ -1,16 +1,32 @@
-import { GraphQLInt, GraphQLList } from 'graphql';
-import { getAllProductsController } from '../../controllers/product.controller';
 import { GraphQLDefaultFieldConfig } from '../typeDefs/general.typeDef';
-import { GraphQLProductType } from '../typeDefs/product.typeDef';
+import { validateGetOneProductArgs } from '../validators/product.validator';
+import {
+  getAllProductsController,
+  getOneProductController,
+} from '../../controllers/product.controller';
+import {
+  GraphQLGetAllProductsArgsType,
+  GraphQLGetOneProductArgsType,
+  GraphQlProductListType,
+  GraphQLProductType,
+} from '../typeDefs/product.typeDef';
 
 // Get All Products query
 export const GET_ALL_PRODUCTS: GraphQLDefaultFieldConfig = {
-  type: new GraphQLList(GraphQLProductType),
-  args: {
-    page: { type: GraphQLInt },
-  },
+  type: GraphQlProductListType,
+  args: GraphQLGetAllProductsArgsType,
   async resolve(parent: any, requestArgs: any) {
     let page: number = requestArgs.page ? requestArgs.page : 0;
-    return getAllProductsController(page);
+    return await getAllProductsController(page);
+  },
+};
+
+// Get One Product query
+export const GET_ONE_PRODUCT: GraphQLDefaultFieldConfig = {
+  type: GraphQLProductType,
+  args: GraphQLGetOneProductArgsType,
+  async resolve(pareny: any, requestArgs: any) {
+    const args = validateGetOneProductArgs(requestArgs);
+    return await getOneProductController(args?.productId);
   },
 };
