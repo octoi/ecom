96ea4a494bc475893a233f5e94ccf7d5875871fb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-12-31 04:58:28 +0530

    paths

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,1 +1,10 @@
 export const GRAPHQL_ENDPOINT = 'http://localhost:5000/graphql';
+
+export enum Paths {
+  register = '/account/register',
+  login = '/account/login',
+  settings = '/account/settings',
+  home = '/',
+  terms = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  notFound = '/404',
+}
