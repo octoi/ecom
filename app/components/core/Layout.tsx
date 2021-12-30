47bbdfee6e45b09199b8ce8ac2b15503a7901c06
fadhilsaheer <fadhilsaheer@gmail.com>
2021-12-31 04:59:01 +0530

    Base layout

---
+++ app/components/core/Layout.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Head from 'next/head';
+import { AuthChecker } from './AuthChecker';
+
+interface Props {
+  title?: string;
+  image?: string;
+  description?: string;
+  hideDefaultHeader?: boolean;
+  needMargin?: boolean;
+}
+
+export const Layout: React.FC<Props> = ({
+  children,
+  title,
+  description,
+  hideDefaultHeader,
+  needMargin,
+  image,
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{title ? title : 'Ecom'}</title>
+        <link rel='icon' href={image ? image : '/favicon.ico'} />
+        <meta
+          name='description'
+          content={description ? description : 'An E-commerce app'}
+        />
+        <meta property='og:title' content={title} />
+        <meta property='og:description' content={description} />
+        <meta property='og:image' content={image} />
+        <meta property='og:type' content='website' />
+      </Head>
+      <div className='p-5 text-white'>
+        {/* {!hideDefaultHeader && <Header />} */}
+        {needMargin && <div className='w-full h-24'></div>}
+        <AuthChecker>{children}</AuthChecker>
+      </div>
+    </div>
+  );
+};
