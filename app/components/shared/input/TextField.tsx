b0fd166e53be7d554f18620c5b97f2f1795878df
fadhilsaheer <fadhilsaheer@gmail.com>
2022-01-01 22:08:54 +0530

    Text field component

---
+++ app/components/shared/input/TextField.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface Props {
+  type?: React.HTMLInputTypeAttribute | undefined;
+  placeholder: string;
+  className?: string;
+  value: string | number | readonly string[] | undefined;
+  onChange: (val: string) => void;
+}
+
+export const TextField: React.FC<Props> = ({
+  type,
+  placeholder,
+  className,
+  value,
+  onChange,
+}) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      className={`w-full mt-2 p-3 rounded outline-none text-xl border-2 focus:border-slate-900 ${className}`}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
+};
