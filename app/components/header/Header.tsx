a27f6bb892d62c9d18343623287c29bd4e349467
fadhilsaheer <fadhilsaheer@gmail.com>
2022-01-02 08:19:12 +0530

    redesigned header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,46 +1,33 @@
 import React from 'react';
 import Link from 'next/link';
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 import { Paths } from '@/utils/constants';
+import { SearchBar } from './SearchBar';
+import { LargeScreenRHS } from './LargeScreenRHS';
+import { SmallScreenRHS } from './SmallScreenRHS';
 
 export const Header = () => {
   return (
-    <div className='bg-slate-50 px-4 py-6 flex items-center justify-between'>
-      <div className='flex items-center'>
-        <Link passHref href={Paths.home}>
-          <p className='text-black font-medium text-xl cursor-pointer'>
-            Ecom 🛒
-          </p>
-        </Link>
-        <Link passHref href={Paths.terms}>
-          <p className='text-black text-xl cursor-pointer ml-5 hidden lg:block'>
-            Terms & Policies
-          </p>
-        </Link>
-      </div>
-      <div className='flex-grow mx-5'>
-        <input
-          type='text'
-          placeholder='Search'
-          className='w-full outline-none px-3 py-2 text-xl'
-        />
-      </div>
-      <div className='items-center hidden md:flex'>
-        <Link passHref href={Paths.login}>
-          <button className='px-5 py-2 bg-slate-900 rounded text-white transition-all hover:opacity-80'>
-            Login
-          </button>
-        </Link>
-        <Link passHref href={Paths.register}>
-          <button className='ml-2 px-5 py-2 bg-slate-900 rounded text-white transition-all hover:opacity-80'>
-            Register
-          </button>
-        </Link>
-      </div>
-      <div className='flex items-center md:hidden'>
-        <button className='ml-2 px-5 py-2 bg-slate-900 rounded text-white transition-all hover:opacity-80'>
-          Popup
-        </button>
-      </div>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static' className='bg-slate-900'>
+        <Toolbar>
+          <Link href={Paths.home} passHref>
+            <Typography
+              variant='h6'
+              noWrap
+              component='div'
+              sx={{ display: { xs: 'none', sm: 'block' } }}
+              className='cursor-pointer'
+            >
+              ECOM
+            </Typography>
+          </Link>
+          <SearchBar />
+          <Box sx={{ flexGrow: 1 }} />
+          <LargeScreenRHS />
+          <SmallScreenRHS />
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
