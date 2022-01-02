08b22c316e145e422d62fea3a6f4eda35c63cc4c
fadhilsaheer <fadhilsaheer@gmail.com>
2022-01-02 13:30:28 +0530

    removed app

--- app/components/core/ApolloWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { ApolloProvider } from '@apollo/client';
-import { getApolloClient } from '@/utils/apolloClient';
-import React from 'react';
-
-export const ApolloWrapper: React.FC = ({ children }) => {
-  const client = getApolloClient();
-
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
-};
