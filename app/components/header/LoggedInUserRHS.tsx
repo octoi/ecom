8d011a32491707469f1def2e76486f2a7425eafd
fadhilsaheer <fadhilsaheer@gmail.com>
2022-01-05 04:50:03 +0530

    drop down items

--- app/components/header/LoggedInUserRHS.tsx
+++ app/components/header/LoggedInUserRHS.tsx
@@ -1,21 +1,46 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Paths } from '@/types/constant.type';
-import { Dropdown, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
 import { UserType } from '@/types/user.type';
+import { logoutUser } from '@/utils/user.util';
 
 export const LoggedInUserRHS: React.FC<{ user: UserType }> = ({ user }) => {
+  const router = useRouter();
+
+  const logout = () => {
+    logoutUser();
+    router.push(Paths.login);
+  };
+
   return (
     <div>
       <Dropdown className='d-inline mx-2'>
-        <Dropdown.Toggle id='dropdown-autoclose-true'>
+        <Dropdown.Toggle
+          id='dropdown-autoclose-true'
+          className='bg-black border-none'
+        >
           {user.name}
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-          <Dropdown.Item href='#'>Menu Item</Dropdown.Item>
-          <Dropdown.Item href='#'>Menu Item</Dropdown.Item>
-          <Dropdown.Item href='#'>Menu Item</Dropdown.Item>
+          <Dropdown.Item>
+            <Link href={`/user/${user?.email}`}>Profile</Link>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <Link href={Paths.settings}>Settings</Link>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <Link href={Paths.chats}>Chats</Link>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <Link href={Paths.newProduct}>New Product</Link>
+          </Dropdown.Item>
+          <Dropdown.Item onClick={logout} className='text-red-500'>
+            Logout
+          </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </div>
