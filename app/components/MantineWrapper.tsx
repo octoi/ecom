b9f147ccb8e7ed58cde5d130a6979702b08617fe
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-23 09:10:32 +0530

    mantine colorscheme wrapper

---
+++ app/components/MantineWrapper.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { useColorScheme } from '@mantine/hooks';
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from '@mantine/core';
+
+export const MantineWrapper: React.FC = ({ children }) => {
+  const systemColorScheme = useColorScheme(); // get system colorscheme (`dark` || `light`)
+
+  const [colorScheme, setColorScheme] =
+    useState<ColorScheme>(systemColorScheme);
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme }}
+      >
+        {children}
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+};
