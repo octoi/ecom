82979108efd094270295e8ca1c125f38f76b8cef
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-23 07:20:49 +0530

    Mantine Setup

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ApolloWrapper } from '@/components/ApolloWrapper';
+import { MantineProvider } from '@mantine/core';
+import { useColorScheme } from '@mantine/hooks';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const colorScheme = useColorScheme(); // get system colorscheme (`dark` || `light`)
+
+  return (
+    <ApolloWrapper>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ApolloWrapper>
+  );
 }
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,12 @@
+import { Layout } from '@/components/Layout';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>;
+  return (
+    <Layout>
+      <div>Hello World</div>
+    </Layout>
+  );
 };
 
 export default Home;
