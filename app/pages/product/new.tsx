95d64f95dde9e45bfd767e9231f7c42b202dc117
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-24 13:39:54 +0530

    Fix: all bugs on loggin in & logout

---
+++ app/pages/product/new.tsx
@@ -0,0 +1,12 @@
+import { Layout } from '@/components/Layout';
+import type { NextPage } from 'next';
+
+const NewProduct: NextPage = () => {
+  return (
+    <Layout title='Sell' description='Lets sell your products with others'>
+      <div>New Product</div>
+    </Layout>
+  );
+};
+
+export default NewProduct;
