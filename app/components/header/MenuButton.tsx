ba7c953a943147f76a05d36b0ba8df9ee0a9a842
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-24 13:14:04 +0530

    showing menu according to user

--- app/components/header/MenuButton.tsx
+++ app/components/header/MenuButton.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { userStore } from '@/state/user.state';
 import { GuestUser } from './GuestUser';
+import { LoggedInUser } from './LoggedInUser';
 
 export const MenuButton = () => {
-  return <GuestUser />;
+  const user = userStore.get();
+
+  return user ? <LoggedInUser user={user} /> : <GuestUser />;
 };
