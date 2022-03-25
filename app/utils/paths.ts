89d4c920109e1ec544eb7c02c50f36ec6e43b508
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-25 09:27:17 +0530

    removed search from menu bar

--- app/utils/paths.ts
+++ app/utils/paths.ts
@@ -7,7 +7,6 @@ export enum Paths {
   product = '/product', // add `/{productID}`
   user = '/user', // add `/{userEmail}`
   chat = '/chat',
-  search = '/search',
   terms = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
   notFound = '/404',
 }
