2ea1411fc3cbe9facaeb63d3afe46f80b15dbaae
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-25 10:07:20 +0530

    FIx: ton og f*cking bugs

--- app/components/home/index.tsx
+++ app/components/home/index.tsx
@@ -1,20 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { getProductStoreItems } from '@/state/product.state';
+import { productStore } from '@/state/product.state';
 import { ProductType } from '@/utils/types';
 import { Products } from './Products';
 import { Search } from './Search';
+import { useQuery } from '@apollo/client';
+import { GET_ALL_PRODUCTS } from './queries';
 
 export const HomeContent: React.FC = () => {
+  const [page, setPage] = useState(1);
   const [products, setProducts] = useState<ProductType[]>([]);
 
+  const { loading, data, error, refetch } = useQuery(GET_ALL_PRODUCTS);
+
   useEffect(() => {
-    setProducts(getProductStoreItems());
-  }, []);
+    if (page === 1 && data) {
+      productStore.set(data.getAllProducts);
+      setProducts(data.getAllProducts);
+    }
+  }, [data, page]);
 
   return (
     <div>
       <Search setProducts={setProducts} />
-      <Products products={products} />
+      <Products
+        products={products}
+        page={page}
+        setPage={setPage}
+        loading={loading}
+        data={data}
+        refetch={refetch}
+        error={error}
+      />
     </div>
   );
 };
