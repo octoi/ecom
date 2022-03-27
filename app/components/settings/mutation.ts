c17a9d829afc1aba0e266aaec31d1649c53981a0
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-27 09:34:06 +0530

    update user mutation

---
+++ app/components/settings/mutation.ts
@@ -0,0 +1,23 @@
+import { gql } from '@apollo/client';
+
+export const UPDATE_USER = gql`
+  mutation (
+    $newName: String
+    $newEmail: String
+    $newProfile: String
+    $newPassword: String
+  ) {
+    updateUser(
+      newName: $newName
+      newEmail: $newEmail
+      newProfile: $newProfile
+      newPassword: $newPassword
+    ) {
+      id
+      token
+      name
+      email
+      profile
+    }
+  }
+`;
