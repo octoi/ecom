c34c5fde75ac54025c9421ccbbab6e7043c3aaed
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-28 08:24:10 +0530

    MyProducts

--- app/pages/account/settings.tsx
+++ app/pages/account/settings.tsx
@@ -2,12 +2,18 @@ import type { NextPage } from 'next';
 import { Layout } from '@/components/Layout';
 import { EditUser } from '@/components/settings/EditUser';
 import { Container, Title } from '@mantine/core';
+import { MyProducts } from '@/components/settings/MyProducts';
+
 const SettingsPage: NextPage = () => {
   return (
     <Layout title='Settings'>
       <Container p='lg'>
-        <Title>Settings</Title>
+        <Title className='mb-10'>Settings</Title>
         <EditUser />
+        <Title order={3} className='my-5'>
+          My Products
+        </Title>
+        <MyProducts />
       </Container>
     </Layout>
   );
